6ce4c11bc223c613eb04e1b3c343bf9d2e5a5bb1
IsaacWSL <isaacmorrowofficial@gmail.com>
2024-08-02 19:14:02 -0700

    Added chroma code highlighting

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module goblog
 
 go 1.22.5
 
-require github.com/gomarkdown/markdown v0.0.0-20240730141124-034f12af3bf6
-
 require (
-	github.com/alecthomas/chroma v0.10.0 // indirect
-	github.com/dlclark/regexp2 v1.11.2 // indirect
+	github.com/alecthomas/chroma v0.10.0
+	github.com/gomarkdown/markdown v0.0.0-20240730141124-034f12af3bf6
 )
+
+require github.com/dlclark/regexp2 v1.11.2 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -10,20 +11,98 @@ import (
 	"path"
 	"strconv"
 
+	chromaHTML "github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
+
 	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
 )
 
+// renderParagraph handles the rendering of paragraph nodes
+func renderParagraph(w io.Writer, _ *ast.Paragraph, entering bool) {
+	if entering {
+		io.WriteString(w, "<p>")
+	} else {
+		io.WriteString(w, "</p>")
+	}
+}
+
+// Render a code block using Chroma to add styling.
+func renderCodeBlock(w io.Writer, c *ast.CodeBlock, entering bool) {
+	if entering {
+		// Extract the programming language from the Info field
+		language := string(c.Info)
+
+		// Get the appropriate lexer for the language
+		lexer := lexers.Get(language)
+		if lexer == nil {
+			lexer = lexers.Fallback
+		}
+
+		// Use Chroma to highlight the code block
+		iterator, err := lexer.Tokenise(nil, string(c.Literal))
+		if err != nil {
+			io.WriteString(w, `<div class="code-block"><pre><code>`)
+			w.Write(c.Literal)
+			io.WriteString(w, `</code></pre></div>`)
+			return
+		}
+
+		formatter := chromaHTML.New(chromaHTML.WithClasses(true))
+		style := styles.Get("github")
+		if style == nil {
+			style = styles.Fallback
+		}
+
+		// Write the opening tags with the language class
+		if language != "" {
+			io.WriteString(w, fmt.Sprintf(`<div class="code-block"><pre><code class="language-%s">`, language))
+		} else {
+			io.WriteString(w, `<div class="code-block"><pre><code>`)
+		}
+
+		// Write the highlighted code
+		err = formatter.Format(w, style, iterator)
+		if err != nil {
+			w.Write(c.Literal)
+		}
+
+		io.WriteString(w, `</code></pre></div>`)
+	}
+}
+
+// myRenderHook is the custom render hook for handling different node types
+func customRenderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool) {
+	switch n := node.(type) {
+	case *ast.Paragraph:
+		renderParagraph(w, n, entering)
+		return ast.GoToNext, true
+	case *ast.CodeBlock:
+		renderCodeBlock(w, n, entering)
+		return ast.GoToNext, true
+	}
+	return ast.GoToNext, false
+}
+
+func newCustomizedRender() *html.Renderer {
+	opts := html.RendererOptions{
+		RenderNodeHook: customRenderHook,
+	}
+	return html.NewRenderer(opts)
+}
+
 func mdToHTML(md []byte) []byte {
 	// create markdown parser with extensions
-	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock | parser.Tables | parser.FencedCode | parser.Autolink | parser.Strikethrough | parser.SpaceHeadings | parser.HeadingIDs
 	p := parser.NewWithExtensions(extensions)
 	doc := p.Parse(md)
 
 	// create HTML renderer with extensions
-	htmlFlags := html.CommonFlags | html.HrefTargetBlank
-	opts := html.RendererOptions{Flags: htmlFlags}
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank | html.LazyLoadImages
+	opts := html.RendererOptions{Flags: htmlFlags, RenderNodeHook: customRenderHook}
 	renderer := html.NewRenderer(opts)
 
 	return markdown.Render(doc, renderer)
@@ -49,13 +128,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		name = lastPart
 	}
 
+	missingPage, err := template.ParseFiles("components/404.html")
+
 	md, err := os.ReadFile("blog/" + fileName)
 	if err != nil {
-		http.Error(w, "404 Not Found", http.StatusNotFound)
+		missingPage.Execute(w, nil)
 		return
 	}
 
-	htmlContent := template.HTML(mdToHTML(md))
+	html := mdToHTML(md)
+	htmlContent := template.HTML(html)
+
 	tmpl, err := template.ParseFiles("components/template.html")
 
 	if err != nil {
