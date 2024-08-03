4e0f0768ab0401fad9affb1a25c69947af487e68
IsaacWSL <isaacmorrowofficial@gmail.com>
2024-08-03 14:19:50 -0700

    Small style changes

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,12 +1,31 @@
 function toggleExpandCode() {
   const codeElements = document.getElementsByClassName("code-block");
-  console.log("Code elements: ", codeElements);
+  const button = document.getElementById("expand-button");
   Array.from(codeElements).forEach((element) => {
     element.classList.toggle("expand");
   });
+
+  let buttonText = "";
+  Array.from(codeElements).forEach((element) => {
+    if (element.classList.contains("expand")) {
+      buttonText = "Collapse Code";
+    }
+  });
+  if (buttonText === "") {
+    buttonText = "Expand Code";
+  }
+
+  button.innerHTML = buttonText;
 }
 
 function toggleDarkMode() {
   const body = document.body;
   body.classList.toggle("dark-mode");
+
+  const button = document.getElementById("dark-mode-button");
+  if (body.classList.contains("dark-mode")) {
+    button.innerHTML = "Light Mode";
+  } else {
+    button.innerHTML = "Dark Mode";
+  }
 }
